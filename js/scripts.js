ae0be351f74fb5dcfb97b62d0bb6f5d6abdd2d5e
santiago510 <santiagoiannucci38@gmail.com>
2021-10-24 15:18:16 -0300

    carrito de compras pulido con local storage

--- js/scripts.js
+++ js/scripts.js
@@ -25,16 +25,31 @@ function addCarritoItem(e) {
         precio: itemPrecio,
         img: itemImg,
         cantidad: 1
-        
+
     };
     anadirAlCarrito(miProducto);
-    console.log (miProducto);
+    console.log(miProducto);
 };
 
 
 // ---------------- cargando el array-----------------------------
 
 function anadirAlCarrito(miProducto) {
+
+    // ----- el for aumenta la cantidad sin duplicar los items--
+    const inputElemento = modalCarrito.getElementsByClassName('input__elemento')
+
+    for (let i = 0; i < productos.length; i++) {
+        if (productos[i].title.trim() === miProducto.title.trim()) {
+            productos[i].cantidad++;
+            const cantidadProductos = inputElemento[i];
+            cantidadProductos.value++;
+            sumarTotal()
+            console.log(productos);
+            return null;
+        }
+    }
+
     productos.push(miProducto);
     cargarCarrito();
 }
@@ -59,7 +74,70 @@ function cargarCarrito() {
             </td>`;
         tablaItem.innerHTML = contenedor;
         modalCarrito.append(tablaItem);
+
+        tablaItem.querySelector('.delete').addEventListener('click', eliminarItem);
+        tablaItem.querySelector('.input__elemento').addEventListener('change', sumarCantidad);
     })
+    sumarTotal()
+};
+
+
+// --------------- sumar precio total acumulado------------------------
+function sumarTotal() {
+    let total = 0;
+    const itemCartTotal = document.querySelector('.itemCartTotal')
+    productos.forEach((item) => {
+        const precio = Number(item.precio.replace("$", ""));
+        total = total + precio * item.cantidad;
+    });
+    itemCartTotal.innerHTML = `<h6 style="color: #000000;">Total: $${total}</h6>`;
+    guardarLocalStorage();
+}
+
+// --------------- eliminar item ------------------------
+
+function eliminarItem(e) {
+    const botonEliminar = e.target;
+    const item = botonEliminar.closest('.itemCarrito');
+    const itemTitle = item.querySelector('.title').textContent;
+    for (let i = 0; i < productos.length; i++) {
+        if (productos[i].title.trim() === itemTitle.trim()) {
+            productos.splice(i, 1);
+        }
+    }
+    item.remove();
+    sumarTotal();
+}
+
+// --------------- sumar cantidad ------------------------ 
+
+function sumarCantidad(e) {
+    const inputElemento = e.target;
+    const item = inputElemento.closest('.itemCarrito');
+    const itemTitle = item.querySelector('.title').textContent;
+    productos.forEach((item) => {
+        if (item.title.trim() === itemTitle.trim()) {
+            inputElemento.value < 1 ? inputElemento.value = 1 : inputElemento.value;
+            item.cantidad = inputElemento.value;
+            sumarTotal();
+        }
+    })
+    console.log(productos);
+};
+
+// --------------- local storage ------------------------
+
+function guardarLocalStorage() {
+    localStorage.setItem('productos', JSON.stringify(productos));
+}
+
+
+window.onload = function () {
+    const storage = JSON.parse(localStorage.getItem('productos'));
+    if (storage) {
+        productos = storage;
+        cargarCarrito();
+    }
 };
 
 
@@ -69,4 +147,15 @@ function cargarCarrito() {
 const modoOscuro = $('.modoOscuro');
 $('.modoOscuro').on('click', () => {
     $('body').toggleClass('dark')
+    if (document.body.classList.contains('dark')) {
+        localStorage.setItem('modoOscuro', true);
+    } else {
+        localStorage.setItem('modoOscuro', false);
+    }
 });
+
+if (localStorage.getItem('modoOscuro') === 'true') {
+    document.body.classList.add('dark');
+} else {
+    document.body.classList.remove('dark');
+}
